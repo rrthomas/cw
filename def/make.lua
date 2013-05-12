ad547b0cf9e176b012f82395c0e5bee297193df2
Reuben Thomas <rrt@sc3d.org>
2013-05-13 00:01:19 +0100

    Retire make patterns in favour of colormake

--- def/make.lua
+++
@@ -1,9 +0,0 @@
--- Don't color kernel compilation dialogs.
-if not arg["menuconfig"] then
-  match("highlight", "%*%*%*")
-  match("lowlight", "checking")
-  match("highlight", "warning: ")
-  match("error", "error: ")
-  match("error", "Error")
-  match("warning", "Interrupt")
-end
